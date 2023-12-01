29de8cad70199b6a61942f759e104f36cc13c7c1
Ameena <ameenazara3@gmail.com>
2023-12-01 11:08:14 -0500

    Initial commit for new Expo project

---
+++ MealBuddy-1/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "mealbuddy": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
---
+++ MealBuddy-1/index.js
@@ -0,0 +1,5 @@
+import { registerRootComponent } from 'expo';
+import App from './App'; // Replace with the correct path to your main component
+
+// App.js
+registerRootComponent(App);
